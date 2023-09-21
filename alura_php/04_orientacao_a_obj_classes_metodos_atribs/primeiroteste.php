897ba1acfa3c2d3d7bcf998b3ba7b7a138772f4f
jucarlett <carlettju@gmail.com>
2023-09-21 18:46:05 -0300

    up

--- alura_php/04_orientacao_a_obj_classes_metodos_atribs/primeiroteste.php
+++ alura_php/04_orientacao_a_obj_classes_metodos_atribs/primeiroteste.php
@@ -5,9 +5,9 @@ require 'src/Conta.php';
 $primeiraConta = new Conta();
 $primeiraConta->cpfTitular = '123.456.789-10';
 $primeiraConta->nomeTitular = 'Vinicius Dias';
-$primeiraConta->saldo = 500;
+$primeiraConta->saldo = 200;
 
 $segundaConta = new Conta();
 $segundaConta->cpfTitular = '987.654.321-10';
 $segundaConta->nomeTitular = 'Patricia';
-$segundaConta->saldo = 1500;
+$segundaConta->saldo = 300;
--- alura_php/04_orientacao_a_obj_classes_metodos_atribs/src/conta.php
+++ alura_php/04_orientacao_a_obj_classes_metodos_atribs/src/conta.php
@@ -1,9 +1,36 @@
 <?php
- 
-class Conta {
-    public string $cpfTitular;
-    public string $nomeTitular;
-    public float $saldo = 0;
 
-   
+class Conta
+{
+    public $cpfTitular;
+    public $nomeTitular;
+    public $saldo = 0;
+
+    public function sacar(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino)
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
 }
