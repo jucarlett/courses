645c63cccb50de56402c29abf607740a36fe8cbd
Ju Carlett <carlettju@gmail.com>
2024-08-02 12:36:01 -0300

    att

--- assembly-mb/01-a-base/assembly.asm
+++ assembly-mb/01-a-base/assembly.asm
@@ -1,8 +1,13 @@
+;bits 64
+;section .text
+;global assembly
+;assembly:
+  ;mov eax, 777
+  ;ret
+
 bits 64
-
-section .text
-
 global assembly
 assembly:
-  mov eax, 777
+  mov eax, 0x11223344
+  mov ax, 0xaabb
   ret
--- assembly-mb/01-a-base/main.c
+++ assembly-mb/01-a-base/main.c
@@ -5,5 +5,5 @@ int assembly(void);
 int main (void)
 {
     printf("The result is: %d\n", assembly());
-    return 0;
+   return 0;
 }
