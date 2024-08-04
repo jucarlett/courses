9c16535f936dab153c057b79e258f7fdd00a905d
Ju Carlett <carlettju@gmail.com>
2024-08-04 14:04:15 -0300

    att

--- assembly-mb/01-a-base/assembly.asm
+++ assembly-mb/01-a-base/assembly.asm
@@ -1,13 +1,8 @@
-;bits 64
-;section .text
-;global assembly
-;assembly:
-  ;mov eax, 777
-  ;ret
-
 bits 64
+
 global assembly
 assembly:
-  mov eax, 0x11223344
-  mov ax, 0xaabb
+  mov rbx, 5
+  mov rcx, 10
+  lea eax, [rcx + rbx*2 + 5]
   ret
--- assembly-mb/01-a-base/main.c
+++ assembly-mb/01-a-base/main.c
@@ -2,8 +2,8 @@
 
 int assembly(void);
 
-int main (void)
+int main(void)
 {
-    printf("The result is: %d\n", assembly());
-   return 0;
+  printf("Resultado: %d\n", assembly());
+  return 0;
 }
